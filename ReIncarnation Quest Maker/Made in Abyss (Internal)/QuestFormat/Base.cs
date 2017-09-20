2373f29bb064a860bd9318b25511161409f6f7df
Vinoade <vinakilada@gmail.com>
2017-09-21 01:41:10 +0800

    sepetared questformat into multiple bitesized files

---
+++ ReIncarnation Quest Maker/Made in Abyss (Internal)/QuestFormat/Base.cs
@@ -0,0 +1,209 @@
+﻿using ReIncarnation_Quest_Maker.Made_In_Abyss_Internal.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ReIncarnation_Quest_Maker.Made_In_Abyss_Internal.Utility;
+
+namespace ReIncarnation_Quest_Maker.Made_In_Abyss_Internal.QuestFormat
+{
+    public static class QuestVariableExtensions
+    {
+        public static void GenerateFromKV_Iterate(this object ThisObject, KVPairFolder ThisFolder, Action<KVPair, FieldInfo> IterationAction = null)
+        {
+            ThisFolder.Items.ForEach(obj =>
+            {
+                FieldInfo ThisFieldInfo = ThisObject.GetType().GetField(obj.Key);
+                if (ThisFieldInfo != null)
+                {
+                    if (typeof(IConvertible).IsAssignableFrom(ThisFieldInfo.FieldType))
+                    {
+                        ThisFieldInfo.SetValue(ThisObject, Convert.ChangeType(obj.Value, ThisFieldInfo.FieldType));
+                    }
+                }
+                IterationAction?.Invoke(obj, ThisFieldInfo);
+            });
+        }
+
+        public static string ConvertToKVText_Iterate(this object ThisObject, int TabCount, bool ConvertEmptyFields)
+        {
+            string ReturnValue = "";
+            FieldInfo[] AllFields = ThisObject.GetType().GetFields();
+            for (int x = 0; x < AllFields.Length; x++)
+            {
+                if (typeof(IConvertible).IsAssignableFrom(AllFields[x].FieldType))
+                {
+                    string Value;
+                    if (AllFields[x].FieldType == typeof(bool))
+                    {
+                        if ((bool)AllFields[x].GetValue(ThisObject))
+                        {
+                            Value = "true";
+                        }
+                        else
+                        {
+                            Value = "false";
+                        }
+                    }
+                    else
+                    {
+                        Value = Convert.ToString(AllFields[x].GetValue(ThisObject));
+                    }
+                    if (ConvertEmptyFields || Value != "")
+                    {
+                        ReturnValue += new string('\t', TabCount) + "\"" + AllFields[x].Name + "\"\t\"" + Value + "\"" + Environment.NewLine;
+                    }
+                }
+            }
+            return ReturnValue;
+        }
+    }
+
+    public abstract class QuestVariable
+    {
+        public QuestVariableOptionalFields OptionalFields = new QuestVariableOptionalFields();
+
+        public static T GenerateFromKeyValue<T>(KVPair ThisKV) where T : QuestVariable, new()
+        {
+            T ReturnValue = new T();
+            ReturnValue.GenerateFromKV(ThisKV);
+            return ReturnValue;
+        }
+
+        public static List<T> FillFromKeyValueFolder<T>(KVPairFolder Thisfolder) where T : QuestVariable, new()
+        {
+            List<T> ReturnValue = new List<T>();
+            Thisfolder.Items.ForEach(obj =>
+            {
+                ReturnValue.Add(GenerateFromKeyValue<T>(obj));
+            });
+            return ReturnValue;
+        }
+
+        public void GenerateFromKeyValue_Iterate(KVPairFolder ThisKV, Action<KVPair, FieldInfo> ThisInfo = null, Action<KVPair, FieldInfo> ThisOptionalInfo = null)
+        {
+            this.GenerateFromKV_Iterate(ThisKV, ThisInfo);
+            OptionalFields.GenerateFromKeyValue_Iterate(ThisKV, ThisOptionalInfo);
+        }
+
+        public abstract void GenerateFromKV(KVPair ThisKV);
+
+        public abstract string ConvertToText(int TabCount = 0);
+
+        public string ConvertToText_Iterate(int TabCount)
+        {
+            return this.ConvertToKVText_Iterate(TabCount, true) + OptionalFields.ConvertToKVText_Iterate(TabCount, false);
+        }
+
+        public static string PrintEncapsulation(string StringToEncapsulate, int TabCount, string DesiredListName = "", bool PrintListName = false)
+        {
+            string ReturnValue = new string('\t', TabCount) + "{" + Environment.NewLine + StringToEncapsulate + new string('\t', TabCount) + "}" + Environment.NewLine;
+            if (PrintListName)
+            {
+                ReturnValue = new string('\t', TabCount) + "\"" + DesiredListName + "\"" + Environment.NewLine + ReturnValue;
+            }
+            return ReturnValue;
+        }
+
+        public static string PrintListVariables<T>(List<T> ThisList, string DesiredListName, int TabCount = 0, bool AlwaysPrint = false) where T : QuestVariable
+        {
+            if (AlwaysPrint || ThisList.Count > 0)
+            {
+                string StringToEncapsulate = "";
+                ThisList.ForEach(obj => StringToEncapsulate += obj.ConvertToText(TabCount + 1));
+                return PrintEncapsulation(StringToEncapsulate, TabCount, DesiredListName, true);
+            }
+            return "";
+        }
+    }
+
+    public class QuestVariableOptionalFields
+    {
+        public void GenerateFromKeyValue_Iterate(KVPairFolder ThisKV, Action<KVPair, FieldInfo> ThisInfo = null)
+        {
+            this.GenerateFromKV_Iterate(ThisKV, ThisInfo);
+        }
+    }
+
+    public abstract class QuestVariableEditorExternal
+    {
+        public List<Action> OnUpdateList = new List<Action>();
+        public void OnUpdate()
+        {
+            OnUpdateList.ForEach(obj => obj());
+        }
+    }
+
+    public class KVPair : QuestVariable
+    {
+        public string Key;
+        public string Value;
+        public KVPairFolder FolderValue;
+
+        public KVPair() { /*Generates an empty KVPair*/ }
+
+        public KVPair(string Key, string Value)
+        {
+            this.Key = Key;
+            this.Value = Value;
+        }
+
+        public KVPair(string Key, KVPairFolder FolderValue)
+        {
+            this.Key = Key;
+            this.FolderValue = FolderValue;
+        }
+
+        public void Delete()
+        {
+            Key = null;
+            Value = null;
+            if (FolderValue != null)
+            {
+                FolderValue.Delete();
+                FolderValue = null;
+            }
+        }
+
+        public override void GenerateFromKV(KVPair ThisKV)
+        {
+            Key = ThisKV.Key;
+            Value = ThisKV.Value;
+        }
+
+        public override string ConvertToText(int TabCount = 0)
+        {
+            return new string('\t', TabCount) + "\"" + Key + "\"\t\"" + Value + "\"" + Environment.NewLine;
+        }
+    }
+
+    public class UniqueList<T> : List<T>
+    {
+        public Func<T, T, bool> Predicate;
+        public List<Action<UniqueList<T>>> Listeners = new List<Action<UniqueList<T>>>();
+
+        public UniqueList(Func<T, T, bool> Predicate)
+        {
+            this.Predicate = Predicate;
+        }
+
+        public new void Remove(T Item)
+        {
+            base.Remove(Item);
+            Listeners.ForEach(obj => obj(this));
+
+        }
+
+        public new void Add(T Item)
+        {
+            base.Add(Item);
+            Listeners.ForEach(obj => obj(this));
+        }
+
+        public void AddListener(Action<UniqueList<T>> Listener)
+        {
+            Listener(this);
+            Listeners.Add(Listener);
+        }
+    }
+}
